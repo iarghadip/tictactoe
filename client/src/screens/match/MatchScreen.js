8844875789fc4080054dab48b290736c6dc47f76
Arghadip Das <arghadipdas@icloud.com>
2026-03-24 16:00:47 +0530

    updated match screen

--- client/src/screens/match/MatchScreen.js
+++ client/src/screens/match/MatchScreen.js
@@ -9,21 +9,21 @@ export default function MatchScreen({
     elapsed, displayName, onCancel, cancelBtnRef
 }) {
     return (
-        <Layout center>
+        <Layout center className="gap-12">
             <div className="flex flex-col items-center w-full mx-auto matching__container">
-                <div className="flex items-center justify-center flex-col gap-4">
-                    <div className="flex items-center justify-center card-theme card-theme-item">
-                        <div className="flex items-center justify-center matching__hourglass">
-                            <HourglassEmptyOutlinedIcon style={{ fontSize: 32 }} />
-                        </div>
+                <div className="flex items-center justify-center card-theme card-theme-item">
+                    <div className="flex items-center justify-center matching__hourglass">
+                        <HourglassEmptyOutlinedIcon style={{ fontSize: 32 }} />
                     </div>
-                    <NormalText size="1">{displayName}</NormalText>
-                    <CapitalText fill="-1">Finding a random player</CapitalText>
                 </div>
-                <div className="flex items-center justify-center flex-col gap-4">
-                    <CapitalText fill="-1">{elapsed}s</CapitalText>
-                    <RoundButton onClick={onCancel} icon={LogoutIcon} btnRef={cancelBtnRef} danger />
-                </div>
+            </div>
+            <div className="flex items-center justify-center flex-col gap-4">
+                <NormalText size="1">{displayName}</NormalText>
+                <CapitalText fill="-1">Finding a random player</CapitalText>
+            </div>
+            <div className="flex items-center justify-center flex-col gap-4">
+                <CapitalText fill="-1">{elapsed}s</CapitalText>
+                <RoundButton onClick={onCancel} icon={LogoutIcon} btnRef={cancelBtnRef} danger />
             </div>
         </Layout>
     );
